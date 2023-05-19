21c67ac2daeb09b6d9cb2a685f79d0393c1f6bdc
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2023-05-19 21:00:35 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,140 @@
+const express = require('express');
+const mysql = require('mysql2');
+const jwt = require('jsonwebtoken');
+
+const secretKey = '6P8NVeUmFOOaYXDKuhKaN08SRJCqqj';
+
+const users = [
+    {
+        id: 99,
+        username: 'admin',
+        password: '1q2w3e4r',
+    },
+    {
+        id: 100,
+        username: 'player',
+        password: '4r3e2w1q'
+    }
+];
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    // port: 3306,
+    user: 'root',
+    password: 'toor',
+    database: 'database_blog',
+});
+
+conn.connect();
+
+const app = express();
+
+app.use(express.json());
+
+const handlerGetCategories = (req, res, next) => {
+    conn.query('select * from category', {}, function(err, result) {
+        console.log(result);
+        // res.json(result);
+        next();
+    });
+};
+
+const handlerGetArticles = (req, res) => {
+    conn.query('select * from article', {}, function(err, result) {
+        console.log(result);
+        res.json(result);
+    });
+};
+
+const handlerPostArticle = (req, res) => {
+    // const query = req.query;
+    const body = req.body;
+
+    // const query = req.params;
+
+    console.log(body);
+
+    // res.send('ok');
+
+    conn.query(`insert into article (id, judul, konten) values (${body.id}, '${body.judul}', '${body.konten}')`, {}, function(err, result) {
+        console.log(result);
+        res.json(result);
+    });
+};
+
+const middlewareValidation = (req, res, next) => {
+    const body = req.body;
+    if (typeof body.id !== 'number') {
+        res.send('error id harus number');
+    }else if( typeof body.konten !== 'string') {
+        res.send('error konten harus string');
+    }
+
+    next();
+}
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIiLCJpYXQiOjE2ODQ1MDI4MTF9.MfmKK0hdTj-O6SBKVmo1u6C1lOh0XW72X9smgn9eCz8
+const auth = (req, res, next) => {
+    try {
+        const tokenDariUser = req.header('authorization');
+        const decoded = jwt.verify(tokenDariUser, secretKey);
+
+        if (decoded.sub !== 99) {
+            res.status(401);
+            res.send({
+                error: 'anda tidak berhak mengakses ini'
+            })
+    
+            return;
+        }
+    } catch (error) {
+        res.status(401);
+        res.send({
+            error: 'token tidak valid'
+        })
+
+        return;
+    }
+
+    next();
+};
+
+app.post('/login', (req, res) => {
+    const audience = req.header('x-audience');
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const userFound = users.find((user) => user.username === username && user.password === password);
+
+    if (!userFound) {
+        res.status(401);
+        res.send({
+            error: 'username password salah'
+        });
+
+        return;
+    }
+
+    const token = jwt.sign({
+        sub: userFound.id,
+        iss: 'skilvul',
+        aud: audience,
+        exp: parseInt(((new Date()).getTime() / 1000) + 5 * 60 * 60),
+    }, secretKey);
+
+    res.send({
+        token: token
+    });
+});
+
+// GET: /categories
+app.get('/categories', handlerGetCategories);
+
+// GET: /articles
+app.get('/articles', auth, handlerGetArticles);
+
+// url -> endpoint
+// POST: /buat-article
+app.post('/buat-article', middlewareValidation, handlerPostArticle);
+
+app.listen(4400);
